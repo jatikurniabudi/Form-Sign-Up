62352125c672a9f557bf9fd2db2c0f5d437a90d8
jatikurniabudi <30432141+jatikurniabudi@users.noreply.github.com>
2017-10-14 23:32:20 +0700

    Add files via upload

---
+++ hasil.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<table align="center">
+<center><h2>Inputan</h2></center>
+<tr><td>Email</td><td>:</td> <td><?php echo $_POST["email"]; ?></td></tr>
+<tr><td>Fullname</td><td>:</td> <td><?php echo $_POST["fullname"]; ?></td></tr>
+<tr><td>Username</td><td>:</td> <td><?php echo $_POST["username"]; ?></td></tr>
+<tr><td>Password</td><td>:</td> <td><?php echo $_POST["password"]; ?></td></tr>
+
+</table>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+  <head>
+    <title>Registration</title>
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width , initial-scale=1">
+  </head>
+  <body>
+    <div id="bg">
+      <div class="form">
+          <h2>REGISTRATION</h2> 
+
+ <form action="hasil.php" method="post">
+            <input class="itpw" type="email" name="email" placeholder="Email"><br>
+            <input class="itpw" type="text" name="fullname" placeholder="Fullname"><br>
+            <input class="itpw" type="text" name="username" placeholder="Username"><br>
+            <input class="itpw" type="password" name="password" placeholder="Password"><br>
+            <input class="its" type="submit" name="singup" value="SIGN UP">
+          </form>
+
+          <p><a href="#"> Have an account? </p>
+
+      </div>
+    </div>
+
+  </body>
+</html
